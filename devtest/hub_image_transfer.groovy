5f3200d08c36970bb3a8dd211f39467df5a38e2a
John Hackett <john.hackett@digital.justice.gov.uk>
2019-01-23 11:52:36 +0000

    Comment in wrong format

--- devtest/hub_image_transfer.groovy
+++ devtest/hub_image_transfer.groovy
@@ -3,4 +3,4 @@ def slurper = new groovy.json.JsonSlurper()
 def buildMatrix = readFileFromWorkspace("seedJob","${System.getenv("SERVER_ENVIRONMENT")}/hub_image_transfer.json")
 def job_script = readFileFromWorkspace("seedJob","${System.getenv("SERVER_ENVIRONMENT")}/hub_image_transfer.pipeline_script")
 
-# Stub
+// Stub
