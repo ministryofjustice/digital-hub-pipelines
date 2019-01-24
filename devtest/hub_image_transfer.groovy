b913611e11ce24cb658ee79c97a0b87115aedda1
John Hackett <john.hackett@digital.justice.gov.uk>
2019-01-24 16:30:19 +0000

    COMMITTING TO MASTER HAHHA

--- devtest/hub_image_transfer.groovy
+++ devtest/hub_image_transfer.groovy
@@ -7,10 +7,21 @@ pipelineJob("hub_image_transfer") {
     }
     concurrentBuild(false)
     parameters {
-        collect(
-   	    choice(name: 'Prison', choices: ['Staging', 'Berwyn', 'Wayland'], description: 'Choose a site to deploy the hub to'),
-   	    choice(name: 'Image', choices: ['Frontend', 'CMS', 'DB', 'Stats'], description: 'Choose a component to upgrade'))
+   	    choiceParam(name: 'Prison', choices: ['Staging', 'Berwyn', 'Wayland'], description: 'Choose a site to deploy the hub to')
+   	    choiceParam(name: 'Image', choices: ['Frontend', 'CMS', 'DB', 'Stats'], description: 'Choose a component to upgrade')
     }
+
+
+/*      parameters {
+        stringParam('target_host', project.host.toUpperCase(), 'Backup target server')
+        stringParam('daily_weekly', period, 'Is this a daily or weekly backup?')
+        booleanParam("run_rman_backup_script", true, "Run the rman backup script")
+        booleanParam("run_azcopy", project.run_azcopy, "Run the azcopy stage or not")
+        booleanParam('checkMode', false, 'Check Mode')
+        stringParam('monoRepoBranch', 'master', 'Ansible Monorepo Branch Name')
+        stringParam('inventoryName', inventoryName, 'Inventory File')
+        stringParam("extraAnsibleArgs", "--diff -v", "ansible-playbook additional parameters")
+      }*/
     env {
 		
     	
