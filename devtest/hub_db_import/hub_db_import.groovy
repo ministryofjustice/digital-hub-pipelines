bda72353768be3361ee83f93a61a0824c6c17d0a
John Hackett <john.hackett@digital.justice.gov.uk>
2019-02-11 17:51:23 +0000

    Adding auth for hub team to jobs

--- devtest/hub_db_import/hub_db_import.groovy
+++ devtest/hub_db_import/hub_db_import.groovy
@@ -22,4 +22,28 @@ pipelineJob("Import Production Database") {
       script(job_script)
     }
   }
+  if (System.getenv('AZURE_AD_DEV_GROUP') != null) {
+    authorization {
+      permissions(System.getenv('AZURE_AD_DEV_GROUP'), [
+        //'com.cloudbees.plugins.credentials.CredentialsProvider.Create',
+        //'com.cloudbees.plugins.credentials.CredentialsProvider.Delete',
+        //'com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains',
+        //'com.cloudbees.plugins.credentials.CredentialsProvider.Update',
+        //'com.cloudbees.plugins.credentials.CredentialsProvider.View',
+        'hudson.model.Item.Build',
+        'hudson.model.Item.Cancel',
+        //'hudson.model.Item.Configure',
+        //'hudson.model.Item.Delete',
+        //'hudson.model.Item.Discover',
+        //'hudson.model.Item.Move',
+        'hudson.model.Item.Read',
+        //'hudson.model.Item.Workspace',
+        //'hudson.model.Run.Delete',
+        'hudson.model.Run.Replay',
+        //'hudson.model.Run.Update',
+        //'hudson.scm.SCM.Tag'
+      ])
+      blocksInheritance()
+    }
+  }
 }
